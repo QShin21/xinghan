f16f1d313bf7e89216244cdd3630b9f3ece8cc47
shin <1670151380@qq.com>
2026-02-25 22:34:13 +0900

    再次修复刚烈

--- pkg/yiwu/init.lua
+++ pkg/yiwu/init.lua
@@ -33,7 +33,7 @@ Fk:loadTranslationTable {
 
 
 -- 夏侯惇，男，魏，4勾玉
-General:new(extension, "xh__xiahoudun", "wei", 4):addSkills { "ganglie", "qingjian" }
+General:new(extension, "xh__xiahoudun", "wei", 4):addSkills { "ex__ganglie", "ex__qingjian" }
 Fk:loadTranslationTable {
   ["xh__xiahoudun"] = "夏侯惇",
   ["#xh__xiahoudun"] = "独眼的罗刹",
