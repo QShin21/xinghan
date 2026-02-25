a355f3b249538b1e52d8a60e6dce6f1744f82ced
shin <1670151380@qq.com>
2026-02-26 03:13:34 +0900

    修复亦算

--- pkg/yiwu/skills/xh__yisuan.lua
+++ pkg/yiwu/skills/xh__yisuan.lua
@@ -1,16 +1,15 @@
 local yisuan = fk.CreateSkill{
-  name = "xh_yisuan",
+  name = "xh__yisuan",
 }
 
 Fk:loadTranslationTable{
-  ["xh_yisuan"] = "亦算",
-  [":xh_yisuan"] = "出牌阶段限一次，当你使用的普通锦囊牌结算结束后，你可以失去1点体力或减1点体力上限，然后获得此牌。",
+  ["xh__yisuan"] = "亦算",
+  [":xh__yisuan"] = "出牌阶段限一次，当你使用的普通锦囊牌结算结束后，你可以失去1点体力或减1点体力上限，然后获得此牌。",
 
-  ["#xh_yisuan-invoke"] = "亦算：是否付出代价获得%arg？",
-  ["#xh_yisuan-choice"] = "亦算：选择代价",
-
-  ["$xh_yisuan1"] = "吾亦能善算谋划。",
-  ["$xh_yisuan2"] = "算计人心，我也可略施一二。",
+  ["#xh__yisuan-invoke"] = "亦算：是否付出代价获得%arg？",
+  ["#xh__yisuan-choice"] = "亦算：选择代价",
+  ["$xh__yisuan1"] = "吾亦能善算谋划。",
+  ["$xh__yisuan2"] = "算计人心，我也可略施一二。",
 }
 
 yisuan:addEffect(fk.CardUseFinished, {
@@ -31,12 +30,12 @@ yisuan:addEffect(fk.CardUseFinished, {
     local room = player.room
     if room:askToSkillInvoke(player, {
       skill_name = yisuan.name,
-      prompt = "#xh_yisuan-invoke:::" .. data.card:toLogString(),
+      prompt = "#xh__yisuan-invoke:::" .. data.card:toLogString(),
     }) then
       local choice = room:askToChoice(player, {
         choices = { "loseHp", "loseMaxHp" },
         skill_name = yisuan.name,
-        prompt = "#xh_yisuan-choice",
+        prompt = "#xh__yisuan-choice",
       })
       event:setCostData(self, { choice = choice, card_id = data.card.id })
       return true
